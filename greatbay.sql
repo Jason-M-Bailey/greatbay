f8d528d8db1225339bea1182f8489fd966f1aac7
Jason Bailey <jmbailey816@gmail.com>
2021-07-13 21:17:24 -0400

    update

--- greatbay.sql
+++ greatbay.sql
@@ -6,14 +6,17 @@ USE greatbay_db;
 
 CREATE TABLE auctions (
     id INT NOT NULL AUTO_INCREMENT,
-    itemName VARCHAR(100) NULL,
+    item_name VARCHAR(100) NULL,
     description VARCHAR(100) NULL,
     starting_bid INT,
     highest_bid INT,
     PRIMARY KEY (id)
 );
 
-INSERT INTO items (itemName, description, starting_bid, highest_bid)
+INSERT INTO auctions (item_name, description, starting_bid, highest_bid)
 VALUES ("My Old Yacht", "Its a Yacht", 1000000, 1000000000);
 
-SELECT * FROM items;
+INSERT INTO auctions (item_name, description, starting_bid, highest_bid)
+VALUES ("Sports Car", "Small and fast", 100000, 300000);
+
+SELECT * FROM auctions;	
